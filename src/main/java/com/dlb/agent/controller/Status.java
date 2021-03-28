02e6cd17d79c3e82590b150bd89a704a232ffc0c
Stan cao <stan.github@gmail.com>
2021-03-28 12:42:58 -0400

    add shell script for cpu and memory

--- src/main/java/com/dlb/agent/controller/Status.java
+++ src/main/java/com/dlb/agent/controller/Status.java
@@ -1,12 +1,26 @@
 package com.dlb.agent.controller;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.stream.Collectors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Status {
   @GetMapping("/hello")
-  public String hello() {
+  public String hello(){
     return "hello";
   }
+
+  @GetMapping("/cpu")
+  public String cpu() throws IOException {
+    Process process =  Runtime.getRuntime().exec("src/main/resources/status.sh");
+    BufferedReader br = new BufferedReader(new InputStreamReader(process.getInputStream()));
+    String response = br.lines().collect(Collectors.joining("\n"));
+    br.close();
+    return response;
+  }
 }
+
